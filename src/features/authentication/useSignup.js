2fa726fd1521bd86b6311cd57a30e00aee8f4fb4
hamodi0-0 <mh9988527@gmail.com>
2025-12-22 16:15:43 +0200

    Sign-up users functionality

---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,15 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signupApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useSignup() {
+  const { mutate: signup, isPending: isLoading } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (data) => {
+      console.log(data);
+      toast.success("Successfully created account");
+    },
+  });
+
+  return { signup, isLoading };
+}
